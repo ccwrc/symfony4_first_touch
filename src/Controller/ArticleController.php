0bd77e44b0c6513dedee0c011784b5e585967c1c
ccwrc <j.muszelik@gmail.com>
2018-02-18 17:25:53 +0100

    first markdown

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,7 +29,7 @@ class ArticleController extends AbstractController {
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug) {
+    public function show($slug, MarkdownInterface $markdown) {
 
         $comments = [
             "comment 1 ytytyyt uyuu yuyuy yuuy gh ghg ",
@@ -38,10 +39,17 @@ class ArticleController extends AbstractController {
 
         // dump($slug, $this);
 
+        $articleContent = <<<EOF
+Sed ut **perspiciatis** unde omnis [iste](http://www.zakazanaplaneta.pl/) natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+EOF;
+
+        $articleContent = $markdown->transform($articleContent);
+
         return $this->render("article/show.html.twig", [
             "title" => ucwords(str_replace("-", " ", $slug)),
             "comments" => $comments,
             "slug" => $slug,
+            "articleContent" => $articleContent,
         ]);
     }
 
