bbca6ef3d82f9d1b1588183788b45110671a2bcb
ccwrc <j.muszelik@gmail.com>
2018-02-23 17:17:40 +0100

    first log

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -9,12 +9,18 @@
 namespace App\Service;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper{
 
     private $cache;
     private $markdown;
+    /**
+     * auto ini - $logger -> alt+enter (-> init fields)
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * auto-generate phpdoc in phpstorm -> alt+enter
@@ -22,9 +28,11 @@ class MarkdownHelper{
      * @param AdapterInterface $cache
      * @param MarkdownInterface $markdown
      */
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown) {
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown,
+                                LoggerInterface $logger) {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
     /**
@@ -32,6 +40,10 @@ class MarkdownHelper{
      * @return string
      */
     public function parse(string $source): string {
+        if(strpos($source, 'bacon') !== false) {
+            $this->logger->info('bekon na sniadanie');
+        }
+
         $item = $this->cache->getItem("markdown_".md5($source));
         if(!$item->isHit()) {
             $item->set($this->markdown->transform($source));
